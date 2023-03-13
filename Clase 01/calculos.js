c4a3ee4bff8811dcccf61600191d565492faae1f
Brian Lara <brian.lara@outlook.com.ar>
2023-03-13 10:48:34 -0300

    Clase 01

---
+++ Clase 01/calculos.js
@@ -0,0 +1,9 @@
+let numero = 7 // crear variables mutables
+const numero2 = 17 // crear variables inmutables
+
+numero = 8
+
+numero2 = 18 // Error: Assignment to constant variable.
+
+console.log("Numero 1:", numero) // 8
+console.log("Numero 2: ", numero2) // 17
